1db9bdac04ebf551307e2361a4e5bda22eb7c11e
Jesus Fernando Almanza Loredo <winxpfercho@live.com.mx>
2020-05-15 02:38:42 -0500

    [FIXED] Login Page

--- Areas/Identity/Data/tienda_webUsers.cs
+++ Areas/Identity/Data/tienda_webUsers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,20 @@ namespace tienda_web.Areas.Identity.Data
     // Add profile data for application users by adding properties to the tienda_webUsers class
     public class tienda_webUsers : IdentityUser
     {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Nombres { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string ApPat { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string ApMat { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(15)")]
+        public string RFC { get; set; }
     }
 }
