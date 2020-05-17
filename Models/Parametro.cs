7df0c3421d17ba8254b7bbe5385d161dfc91358d
Jesus Fernando Almanza Loredo <winxpfercho@live.com.mx>
2020-05-16 19:53:57 -0500

    [FIXED] PDF Generator

---
+++ Models/Parametro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tienda_web.Models
+{
+    public class Parametro
+    {
+        public int ParametroId { get; set; }
+        public string VcParamName { get; set; }
+        public string VcParamValue { get; set; }
+        public string VcParamStatus { get; set; }
+    }
+}
